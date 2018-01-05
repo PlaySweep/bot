6174b07e4076e33cfe3b6afaa0c1b3c04339f384
Ryan Waits <ryanwaits@gmail.com>
2018-01-05 00:33:13 -0600

    add mulligans

--- bot/commands/commands.rb
+++ bot/commands/commands.rb
@@ -198,7 +198,8 @@ module Commands
       say text, quick_replies: [["Earn mulligans", "Earn mulligans"], ["Make more picks", "Select picks"]]
       next_command :status
     when 'Earn mulligans'
-      say "Mulligans run...", quick_replies: [["Invite", "Invite"]]
+      show_invite
+      stop_thread
     when 'Games'
       text = "Choose from the sports below 👇"
       say text, quick_replies: [["Up next (#{user.session[:upcoming].count})", "Up next"], ["Live (#{user.session[:in_progress].count})", "Live"], ["Completed (#{user.session[:current].count})", "Completed"], ["Select Picks", "Select picks"]]
