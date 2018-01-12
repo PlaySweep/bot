df8c7b9fc252e24db5508ccf89c194a1b2c52852
Ryan Waits <ryan.waits@gmail.com>
2018-01-11 19:56:47 -0600

    replace id with facebook_uuid

--- bot/commands/commands.rb
+++ bot/commands/commands.rb
@@ -13,10 +13,10 @@ module Commands
     referred = user["referral_data"]["referred"]
     if @new_user && !referred
       update_sender(postback.referral.ref, referral_count)
-      update_recipient user["id"]
+      update_recipient user["facebook_uuid"]
     else
       update_sender(postback.referral.ref, referral_count)
-      update_recipient user["id"]
+      update_recipient user["facebook_uuid"]
     end
     text = "Welcome to Sweep #{user["first_name"]}!\n\nWe’re giving away $50 worth of Amazon gift cards every game day. Predict 4 games in a row and win your piece of the pie!"
     say text, quick_replies: [["How to play", "How to play"], ["Select picks", "Select picks"]]
