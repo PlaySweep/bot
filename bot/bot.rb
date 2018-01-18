965e5d027c01ba360c4cb5cb07f10bd1094c7bdd
Ryan Waits <ryan.waits@gmail.com>
2018-01-17 20:00:21 -0600

    account for the word play

--- bot/bot.rb
+++ bot/bot.rb
@@ -55,6 +55,7 @@ Rubotnik.route :message do |request|
   bind 'get', 'more', 'picks', all: true, to: :more_picks
   bind 'how', 'to', 'play', to: :how_to_play
   bind 'select', 'picks', all: true, to: :select_picks
+  bind 'play', to: :select_picks
   bind 'update', 'picks', all: true, to: :select_picks
   # bind 'use', 'mulligan', all: true, to: :use_mulligan, reply_with: {
   #   text: "Alright #{@graph_user["first_name"]}, you have 1 mulligan to use. Do you want to use it now to keep your streak alive?",
