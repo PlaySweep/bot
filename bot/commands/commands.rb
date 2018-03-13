44ac7c747576541db706d8305026030497df9c6c
Ryan Waits <ryan.waits@gmail.com>
2018-03-13 04:51:21 -0500

    rescue status image call

--- bot/commands/commands.rb
+++ bot/commands/commands.rb
@@ -382,42 +382,53 @@ module Commands
 
   def my_picks
     $api.find_or_create('users', user.id)
-    # if $api.user.images.any?
-    #   if $api.user.data.status_changed
-    #     set('status changed', user.id)
-    #     message.typing_on
-    #     say "Brb, fetching the rest of your picks ⏳"
-    #     message.typing_on
-    #     $api.for_picks('status')
-    #     quick_replies = [
-    #       { content_type: 'text', title: "Select picks", payload: "Select picks" },
-    #       { content_type: 'text', title: "Status", payload: "Status" }
-    #     ]
-    #     show_media($api.user.images.for_status, quick_replies)
-    #     stop_thread
-    #   else
-    #     message.typing_on
-    #     quick_replies = [
-    #       { content_type: 'text', title: "Select picks", payload: "Select picks" },
-    #       { content_type: 'text', title: "Status", payload: "Status" }
-    #     ]
-    #     show_media($api.user.images.for_status, quick_replies)
-    #     stop_thread
-    #   end
-    # else
-    #   set('status changed', user.id)
-    #   message.typing_on
-    #   say "Brb, fetching the rest of your picks ⏳"
-    #   message.typing_on
-    #   $api.for_picks('status')
-    #   quick_replies = [
-    #     { content_type: 'text', title: "Select picks", payload: "Select picks" },
-    #     { content_type: 'text', title: "Status", payload: "Status" }
-    #   ]
-    #   show_media($api.user.images.for_status, quick_replies)
-    #   stop_thread
-    # end
-    stop_thread
+    if $api.user.images.any?
+      if $api.user.data.status_changed
+        set('status changed', user.id)
+        begin
+          message.typing_on
+          say "Brb, fetching the rest of your picks ⏳"
+          message.typing_on
+          $api.for_picks('status')
+          quick_replies = [
+            { content_type: 'text', title: "Select picks", payload: "Select picks" },
+            { content_type: 'text', title: "Status", payload: "Status" }
+          ]
+          show_media($api.user.images.for_status, quick_replies)
+          stop_thread
+        rescue Facebook::Messenger::FacebookError => e
+          say "Whoops, I screwed up. Gimme a sec, I'll try again..."
+          # send an alert message
+          stop_thread
+        end
+      else
+        message.typing_on
+        quick_replies = [
+          { content_type: 'text', title: "Select picks", payload: "Select picks" },
+          { content_type: 'text', title: "Status", payload: "Status" }
+        ]
+        show_media($api.user.images.for_status, quick_replies)
+        stop_thread
+      end
+    else
+      set('status changed', user.id)
+      begin
+        message.typing_on
+        say "Brb, fetching the rest of your picks ⏳"
+        message.typing_on
+        $api.for_picks('status')
+        quick_replies = [
+          { content_type: 'text', title: "Select picks", payload: "Select picks" },
+          { content_type: 'text', title: "Status", payload: "Status" }
+        ]
+        show_media($api.user.images.for_status, quick_replies)
+        stop_thread
+      rescue Facebook::Messenger::FacebookError => e
+        say "Whoops, I screwed up. Gimme a sec, I'll try again..."
+        # send an alert message
+        stop_thread
+      end
+    end
   end
 
   def select_picks
