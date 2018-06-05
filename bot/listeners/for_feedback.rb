39134fdcd8160dd276561c6aac201c8555b09502
Ryan Waits <ryan.waits@gmail.com>
2018-06-04 19:02:43 -0500

    remove duplicate response to send feedback

--- bot/listeners/for_feedback.rb
+++ bot/listeners/for_feedback.rb
@@ -1,8 +1,8 @@
 def listen_for_feedback
   stop_thread and return if message.text.nil?
-  keywords = ['feedback', 'send feedback', 'got questions?', 'help']
+  keywords = ['help', 'help?']
   msg = message.text.split(' ').map(&:downcase)
-  multiple_keywords, multiple_msg = ['got questions?', 'send feedback', 'need help?'], message.text.split(' ').permutation(2).to_a.map { |m| m.join(' ').downcase } 
+  multiple_keywords, multiple_msg = ['got questions?', 'send feedback'], message.text.split(' ').permutation(2).to_a.map { |m| m.join(' ').downcase } 
   matched, double_matched = (keywords & msg), (multiple_keywords & multiple_msg)
   options = ["I hope you aren't having too many issues 😢", "I totes love feedback 🤗, the constructive kind 🙏"]
   bind keywords, to: :entry_to_feedback, reply_with: {
