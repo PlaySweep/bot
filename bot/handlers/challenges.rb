3b56847c295d11188f86ad361a9d64ff5af257db
Ryan Waits <ryan.waits@gmail.com>
2018-09-03 18:01:14 -0500

    update challenges webview button

--- bot/handlers/challenges.rb
+++ bot/handlers/challenges.rb
@@ -1,33 +1,35 @@
 module Commands
   def handle_show_challenges
     quick_replies = [{ content_type: 'text', title: "Make picks", payload: "SELECT PICKS" }, { content_type: 'text', title: "Status", payload: "STATUS" }, { content_type: 'text', title: "Preferences", payload: "PREFERENCES" }]
-    url = "#{ENV['WEBVIEW_URL']}/contests"
-    show_carousel(resources, quick_replies)
+    url = "#{ENV['WEBVIEW_URL']}/#{user.id}/contests"
+    @contests = Sweep::Contest.all
+    @contest = @contests.first
+    show_button("Enter now!", "We have #{@contests.length} tournaments available!\n\n⭐️ FEATURED ⭐️\n - #{@contest.name}", quick_replies, url)
     stop_thread
   end
 
-  def resources
-    @contests = Sweep::Contest.all
-    if @contests.empty?
-      say "No challenges available.", quick_replies: ["Make picks"]
-      stop_thread
-    else
-        @contest = @contests.first
-        [
-         {
-          title: "⭐️ FEATURED ⭐️",
-          image_url: "http://www.playsweep.com/images/logo.png",
-          subtitle: "#{@contest.name}",
-          buttons: [
-            {
-              type: "web_url",
-              url: "#{ENV['WEBVIEW_URL']}/#{user.id}/contests",
-              title: "View Contests",
-              webview_height_ratio: "full"
-            }           
-          ]      
-        }
-      ]
-    end
-  end
+  # def resources
+  #   @contests = Sweep::Contest.all
+  #   if @contests.empty?
+  #     say "No challenges available.", quick_replies: ["Make picks"]
+  #     stop_thread
+  #   else
+  #       @contest = @contests.first
+  #       [
+  #        {
+  #         title: "⭐️ FEATURED ⭐️",
+  #         image_url: "http://www.playsweep.com/images/logo.png",
+  #         subtitle: "#{@contest.name}",
+  #         buttons: [
+  #           {
+  #             type: "web_url",
+  #             url: "#{ENV['WEBVIEW_URL']}/#{user.id}/contests",
+  #             title: "View Contests",
+  #             webview_height_ratio: "full"
+  #           }           
+  #         ]      
+  #       }
+  #     ]
+  #   end
+  # end
 end
