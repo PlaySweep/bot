d468f1300261ecc7217f3f124b2e4e98a4662958
Ryan Waits <ryan.waits@gmail.com>
2018-09-11 07:21:02 -0500

    adjust copy

--- bot/handlers/status.rb
+++ bot/handlers/status.rb
@@ -23,7 +23,7 @@ module Commands
   end
 
   def show_losing_streak
-    text = "Yeah, a losing streak of #{@sweepy.losing_streak} no bueno, but at least you'll get something for 4 of em' in a row 🤑"
+    text = "Yeah, a losing streak of #{@sweepy.losing_streak} is no bueno, but at least you'll get something for 4 of em' in a row 🤑"
     url = "#{ENV['WEBVIEW_URL']}/#{user.id}/status"
     show_button("Show Status", text, nil, url)
     stop_thread
