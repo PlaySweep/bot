ce595ace08f4666b2e779dcd43006a8197cfe42d
Ryan Waits <ryan.waits@gmail.com>
2019-03-03 22:03:58 -0600

    move wit response underneath check for emojis

--- bot/bot.rb
+++ bot/bot.rb
@@ -24,40 +24,40 @@ end
 
 Rubotnik.route :message do
   sweepy = Sweep::User.find_or_create(user.id)
-  # if sweepy.confirmed && sweepy.preference.owner_id  #TODO figure out a way to not call out to api every time to verify if they are confirmed
-  #   unless message.messaging['message']['attachments'] && message.messaging['message']['attachments'].any?
-  #     response = $wit.message(message.text).to_dot
-  #     entity_objects = response.entities
-  #     entities = response.entities.keys
+  if sweepy.confirmed && sweepy.preference.owner_id  #TODO figure out a way to not call out to api every time to verify if they are confirmed
+    unless message.messaging['message']['attachments'] && message.messaging['message']['attachments'].any?
+      response = $wit.message(message.text).to_dot
+      entity_objects = response.entities
+      entities = response.entities.keys
       
-  #     unsubscribe if entities.include?("unsubscribe")
-  #     fetch_picks if entities.include?("make_picks")
-  #     fetch_status if entities.include?("status")
-  #     trigger_invite if entities.include?("share")
-  #     show_how_to_play if entities.include?("how_to_play")
-  #     show_rules if entities.include?("rules")
-  #     show_prizes if entities.include?("prizes")
-  #     send_help if entities.include?("help")
-  #     list_of_commands if entities.include?("commands")
-  #     legal if entities.include?("legal")
-  #     location if entities.include?("local_events")
-  #     team_select if entities.include?("team_select")
-  #     positive_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "positive" && entities.size == 1
-  #     negative_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "negative" && entities.size == 1
-  #     neutral_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "neutral" && entities.size == 1
-  #     default do
-  #       say "Hmm, I do not follow that one..."
-  #       stop_thread
-  #     end unless entities
-  #   end
-  # elsif sweepy.confirmed && !sweepy.preference.owner_id && !entities.include?("team_select")
-  #   prompt_team_select
-  # elsif sweepy.confirmed && !sweepy.preference.owner_id && entities.include?("team_select")
-  #   team_select
-  # else
-  #   confirmation_text = "Please confirm your Budweiser Sweep account below to move forward 👇"
-  #   url = "#{ENV['WEBVIEW_URL']}/#{user.id}/account"
-  #   show_button("Quick Setup ⚡️", confirmation_text, nil, url)
-  #   stop_thread
-  # end
+      unsubscribe if entities.include?("unsubscribe")
+      fetch_picks if entities.include?("make_picks")
+      fetch_status if entities.include?("status")
+      trigger_invite if entities.include?("share")
+      show_how_to_play if entities.include?("how_to_play")
+      show_rules if entities.include?("rules")
+      show_prizes if entities.include?("prizes")
+      send_help if entities.include?("help")
+      list_of_commands if entities.include?("commands")
+      legal if entities.include?("legal")
+      location if entities.include?("local_events")
+      team_select if entities.include?("team_select")
+      positive_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "positive" && entities.size == 1
+      negative_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "negative" && entities.size == 1
+      neutral_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "neutral" && entities.size == 1
+      default do
+        say "Hmm, I do not follow that one..."
+        stop_thread
+      end unless entities
+    end
+  elsif sweepy.confirmed && !sweepy.preference.owner_id && !entities.include?("team_select")
+    prompt_team_select
+  elsif sweepy.confirmed && !sweepy.preference.owner_id && entities.include?("team_select")
+    team_select
+  else
+    confirmation_text = "Please confirm your Budweiser Sweep account below to move forward 👇"
+    url = "#{ENV['WEBVIEW_URL']}/#{user.id}/account"
+    show_button("Quick Setup ⚡️", confirmation_text, nil, url)
+    stop_thread
+  end
 end
