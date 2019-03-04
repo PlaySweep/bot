7788de5b4d327be7e273cba89a617c03277fd01b
Ryan Waits <ryan.waits@gmail.com>
2019-03-03 23:52:04 -0600

    show all rules, terms, policy in one

--- bot/responses/rules.rb
+++ bot/responses/rules.rb
@@ -1,13 +1,12 @@
 def show_rules
-  text = "You can view the official Rules, Terms of Service, and Privacy Policy by tapping below 👇"
   rules_url = "http://www.endemiclabs.co/budweiser-sweep-rules.html"
-  show_button("Rules", text, nil, rules_url)
+  show_button("Rules", "You can view the official Rules by tapping below 👇", nil, rules_url)
 
   terms_url = "https://www.budweiser.com/en/terms-conditions.html"
-  show_button("Terms of Service", "", nil, terms_url)
+  show_button("Terms of Service", "You can view the official Terms of Service by tapping below 👇", nil, terms_url)
 
   privacy_url = "https://www.budweiser.com/en/privacy-policy.html"
-  show_button("Privacy Polcy", "", nil, privacy_url)
+  show_button("Privacy Policy", "You can view the official Privacy Policy by tapping below 👇", nil, privacy_url)
+  
   stop_thread
-
 end
