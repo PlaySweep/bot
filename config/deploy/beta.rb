ddbec47d97fce8b6a26e602d73509d30fbae3603
Ryan Waits <ryan.waits@gmail.com>
2019-04-06 10:25:57 -0500

    update beta branch deploy

--- config/deploy/beta.rb
+++ config/deploy/beta.rb
@@ -4,7 +4,7 @@ server 'sweep_beta', roles: %w{app db web}
 set :deploy_to, "/var/www/sweep_bot"
 set :tmp_dir, '/home/deploy/tmp'
 
-set :branch, 'release-201903'
+set :branch, 'sandbox'
 set :rails_env, 'beta'
 
 set :linked_files, %w{config/application.yml}
