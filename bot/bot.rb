53afd7ac49a6563a22e810a754de3f38ff6e8ff5
Ryan Waits <ryan.waits@gmail.com>
2019-04-19 20:13:53 -0500

    check for any team roles

--- bot/bot.rb
+++ bot/bot.rb
@@ -34,7 +34,7 @@ Rubotnik.route :message do
         entity_objects = response.entities
         entities = response.entities.keys
 
-        if sweepy.roles.first.try(:owner_id)
+        if sweepy.roles.first.any?
           unsubscribe if entities.include?("unsubscribe")
           fetch_picks if entities.include?("make_picks")
           fetch_status if entities.include?("status")
