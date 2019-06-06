f9ffc038f10c8229acefa22850970016f9130f3c
Ryan Waits <ryan.waits@gmail.com>
2019-06-05 19:00:27 -0500

    add prizing elements

--- bot/responses/prizes.rb
+++ bot/responses/prizes.rb
@@ -1,4 +1,25 @@
 def show_prizes
-  say "Prizes change every single day, but the prizes that are available are primarily game tickets and merchandise ⚾️"
+  @sweepy = Sweep::User.find(facebook_uuid: user.id)
+  say "Prizes change every single day for the #{@sweepy.roles.first.abbreviation} depending on the contest, but the prizes that are available are primarily game tickets and merchandise ⚾️"
+  show_carousel(elements: prizing_elements)
   stop_thread
+end
+
+def prizing_elements
+  [
+        {
+        title: "All-Star Contest Prizing",
+        image_url: "https://budweiser-sweep-assets.s3.amazonaws.com/allstar_prizing_logo.png",
+        subtitle: "There are tons of prizes on the line for the All-Star Contest!",
+        buttons: [
+          {
+            type: :web_url,
+            url: "#{ENV["WEBVIEW_URL"]}/prizing/allstar",
+            title: "Prizes",
+            webview_height_ratio: 'full',
+            messenger_extensions: true
+          }
+        ]
+      }
+  ]
 end
