78bbbcfb27e5ad11c30de5fe924d5b0287d5b348
Ryan Waits <ryan.waits@gmail.com>
2019-06-07 21:47:29 -0500

    adjust for allstar link

--- bot/responses/start.rb
+++ bot/responses/start.rb
@@ -8,10 +8,14 @@ def start
         team = postback.referral.ref.split('_').map(&:capitalize).join(' ').split('?')[0]
         param_key = postback.referral.ref.split('?')[-1].split('=')[0]
         source = postback.referral.ref.split('?')[-1].split('=')[1]
-        if param_key == "source"
-          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team, source: source)
+        if team == "All Star"
+          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: source)
         else
-          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team)
+          if param_key == "source"
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team, source: source)
+          else
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team)
+          end
         end
       else
         Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true)
