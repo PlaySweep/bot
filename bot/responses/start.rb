bca87a8977c968d343b5ea51e390d462622fe382
Ryan Waits <ryan.waits@gmail.com>
2019-07-07 09:35:51 -0500

    check ref empty string

--- bot/responses/start.rb
+++ bot/responses/start.rb
@@ -5,16 +5,18 @@ def start
   bind 'START' do
     begin
       if postback.referral.ref
-        team = postback.referral.ref.split('_').map(&:capitalize).join(' ').split('?')[0]
-        param_key = postback.referral.ref.split('?')[-1].split('=')[0]
-        source = postback.referral.ref.split('?')[-1].split('=')[1]
-        if team == "All Star"
-          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: source)
-        else
-          if param_key == "source"
-            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team, source: source)
+        unless postback.referral.ref == ""
+          team = postback.referral.ref.split('_').map(&:capitalize).join(' ').split('?')[0]
+          param_key = postback.referral.ref.split('?')[-1].split('=')[0]
+          source = postback.referral.ref.split('?')[-1].split('=')[1]
+          if team == "All Star"
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: source)
           else
-            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team)
+            if param_key == "source"
+              Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team, source: source)
+            else
+              Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team)
+            end
           end
         end
       else
