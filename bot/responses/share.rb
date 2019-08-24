307d728f9365483f7adbc965e3ce68f1c0096727
Ryan Waits <ryan.waits@gmail.com>
2019-08-24 18:50:06 -0500

    add new route for inviting in the web view

--- bot/responses/share.rb
+++ bot/responses/share.rb
@@ -1,6 +1,6 @@
 def trigger_invite
   sweepy = Sweep::User.find(facebook_uuid: user.id)
-  url = "#{ENV['WEBVIEW_URL']}/challenge/invite/#{sweepy.slug}"
-  show_button("Invite", "Share with your friends by following the button below 👇", nil, url)
+  url = "#{ENV['WEBVIEW_URL']}/invite/#{sweepy.slug}"
+  show_button("Invite", "Share with your friends by tapping below 👇", nil, url)
   stop_thread
 end
