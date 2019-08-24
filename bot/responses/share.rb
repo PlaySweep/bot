9303aba947aa4625eb9e5be3ac0293c807464293
Ryan Waits <ryan.waits@gmail.com>
2019-08-24 17:55:27 -0500

    add sweepy user in share

--- bot/responses/share.rb
+++ bot/responses/share.rb
@@ -1,4 +1,5 @@
 def trigger_invite
+  sweepy = Sweep::User.find(facebook_uuid: user.id)
   url = "#{ENV['WEBVIEW_URL']}/challenge/invite/#{sweepy.slug}"
   show_button("Invite", "Share with your friends by following the button below 👇", nil, url)
   stop_thread
