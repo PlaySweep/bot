2df32b0123ebe7cd4a6da04a5992ec567988ad0b
Ryan Waits <ryan.waits@gmail.com>
2019-08-24 13:18:44 -0500

    syntax error in prizes

--- bot/responses/prizes.rb
+++ bot/responses/prizes.rb
@@ -1,5 +1,5 @@
 def show_prizes
-  @sweepy = Sweep::User.find(facebook_uuid: user.id)
+  sweepy = Sweep::User.find(facebook_uuid: user.id)
   prizing_copy = sweepy.copies.find { |copy| copy.category == "Prizing Info" }
   say prizing_copy.message
   stop_thread
