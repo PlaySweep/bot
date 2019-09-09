701814e8a7e7bbeab5353128146e1ceb07c1eaa3
Ryan Waits <ryan.waits@gmail.com>
2019-09-09 00:13:49 -0500

    add quick reply logic

--- bot/bot.rb
+++ bot/bot.rb
@@ -36,45 +36,49 @@ end
 
 Rubotnik.route :message do
   if message.quick_reply == "START"
+    puts "*" * 25
+    puts "Running ad..."
+    puts "*" * 25
     start
-  end
-  sweepy = Sweep::User.find_or_create(facebook_uuid: user.id)
-  if sweepy.locked
-    say "Sorry #{sweepy.first_name}, you are unable to play at this time."
-    stop_thread
   else
-    if sweepy.confirmed  #TODO figure out a way to not call out to api every time to verify if they are confirmed
-      unless message.messaging['message']['attachments'] && message.messaging['message']['attachments'].any?
-        response = $wit.message(message.text).to_dot
-        entity_objects = response.entities
-        entities = response.entities.keys
-        unsubscribe if entities.include?("unsubscribe")
-        fetch_picks if entities.include?("make_picks")
-        fetch_status if entities.include?("status") unless entities.include?("make_picks")
-        trigger_invite if entities.include?("share")
-        show_how_to_play if entities.include?("how_to_play")
-        show_prizes if entities.include?("prizes")
-        send_help if entities.include?("help")
-        list_of_commands if entities.include?("commands")
-        location if entities.include?("local_events")
-        switch_prompt_message if message.text.split(' ').map(&:downcase).include?("switch") unless entities.include?("status") || entities.include?("prizes")
-        
-        positive_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "positive" && entities.size == 1 unless message.text.split(' ').map(&:downcase).include?("switch") 
-        negative_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "negative" && entities.size == 1 unless message.text.split(' ').map(&:downcase).include?("switch") 
-        neutral_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "neutral" && entities.size == 1 unless message.text.split(' ').map(&:downcase).include?("switch") 
-        default do
-          say "Hmm, I do not follow that one..."
-          stop_thread
-        end unless entities
-      end
+    sweepy = Sweep::User.find_or_create(facebook_uuid: user.id)
+    if sweepy.locked
+      say "Sorry #{sweepy.first_name}, you are unable to play at this time."
+      stop_thread
     else
-      unless message.messaging['message']['attachments'] && message.messaging['message']['attachments'].any?
-        response = $wit.message(message.text).to_dot
-        entities = response.entities.keys
-        if entities.include?("unsubscribe")
-          unsubscribe
-        else
-          account_confirmation
+      if sweepy.confirmed  #TODO figure out a way to not call out to api every time to verify if they are confirmed
+        unless message.messaging['message']['attachments'] && message.messaging['message']['attachments'].any?
+          response = $wit.message(message.text).to_dot
+          entity_objects = response.entities
+          entities = response.entities.keys
+          unsubscribe if entities.include?("unsubscribe")
+          fetch_picks if entities.include?("make_picks")
+          fetch_status if entities.include?("status") unless entities.include?("make_picks")
+          trigger_invite if entities.include?("share")
+          show_how_to_play if entities.include?("how_to_play")
+          show_prizes if entities.include?("prizes")
+          send_help if entities.include?("help")
+          list_of_commands if entities.include?("commands")
+          location if entities.include?("local_events")
+          switch_prompt_message if message.text.split(' ').map(&:downcase).include?("switch") unless entities.include?("status") || entities.include?("prizes")
+          
+          positive_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "positive" && entities.size == 1 unless message.text.split(' ').map(&:downcase).include?("switch") 
+          negative_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "negative" && entities.size == 1 unless message.text.split(' ').map(&:downcase).include?("switch") 
+          neutral_sentiment if entity_objects["sentiment"] && entity_objects["sentiment"].first["value"] == "neutral" && entities.size == 1 unless message.text.split(' ').map(&:downcase).include?("switch") 
+          default do
+            say "Hmm, I do not follow that one..."
+            stop_thread
+          end unless entities
+        end
+      else
+        unless message.messaging['message']['attachments'] && message.messaging['message']['attachments'].any?
+          response = $wit.message(message.text).to_dot
+          entities = response.entities.keys
+          if entities.include?("unsubscribe")
+            unsubscribe
+          else
+            account_confirmation
+          end
         end
       end
     end
