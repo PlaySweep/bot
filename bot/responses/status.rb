456f0fa523072030fda691018cdb452839599054
Ryan Waits <ryan.waits@gmail.com>
2019-09-28 22:16:20 -0500

    send different status message when they have no stats

--- bot/responses/status.rb
+++ bot/responses/status.rb
@@ -4,7 +4,11 @@ def fetch_status
   message.typing_on
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   contest_size = sweepy.latest_stats.size == 1 ? "contest" : "#{sweepy.latest_stats.size} contests"
-  say "You're on a current streak of #{sweepy.stats.current_pick_streak} and have gone #{sweepy.latest_stats.map(&:wins).sum}-#{sweepy.latest_stats.map(&:losses).sum} in your last #{contest_size}."
+  if sweepy.latest_stats.size >= 1
+    say "You're on a current streak of #{sweepy.stats.current_pick_streak} and have gone #{sweepy.latest_stats.map(&:wins).sum}-#{sweepy.latest_stats.map(&:losses).sum} in your last #{contest_size}."
+  else
+    say "You're just getting started, #{sweepy.first_name}! Once you start collecting some stats, I'll show your latest information right here."
+  end
   fetch_picks
   stop_thread
   message.typing_off
