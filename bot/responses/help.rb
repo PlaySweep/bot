35c787853e9445ff142c10f1c6474f0c0e8234b1
Ryan Waits <ryan.waits@gmail.com>
2019-09-30 05:58:29 -0500

    change phone number

--- bot/responses/help.rb
+++ bot/responses/help.rb
@@ -12,8 +12,7 @@ def help
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Status", payload: "STATUS"}]
   say "We're on it, #{sweepy.first_name}. We will reach out to you within the next 24 hours. Thanks for your patience.", quick_replies: quick_replies
-  # phone_number = "2054137379"
-  phone_number = "4805227771"
+  phone_number = "2054137379"
   Popcorn.notify(phone_number, "#{sweepy.account.app_name} Customer Support Alert:\n\n#{sweepy.first_name} #{sweepy.last_name}\n#{sweepy.email}\n#{sweepy.zipcode}")
   stop_thread
   message.typing_off
