bab804a9382ff5e78534ecc202e075ed0988ba8c
Ryan Waits <ryan.waits@gmail.com>
2019-12-06 11:01:27 -0700

    check if referral exists before checking ref

--- bot/responses/start.rb
+++ bot/responses/start.rb
@@ -4,19 +4,23 @@ require 'httparty'
 def start
   bind 'START' do
     begin
-      if postback.referral.ref && postback.referral.ref != ""
-        ref = postback.referral.ref
-        if ref.start_with?("rc")
-          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, referral_code: ref, source: "referred")
-        else
-          ref = ref.split('_').map(&:capitalize).join(' ').split('?')[0]
-          param_key = postback.referral.ref.split('?')[-1].split('=')[0]
-          source = postback.referral.ref.split('?')[-1].split('=')[1]
-          if param_key == "source"
-            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: ref, source: source)
+      if postback.referral
+        if postback.referral.ref && postback.referral.ref != ""
+          ref = postback.referral.ref
+          if ref.start_with?("rc")
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, referral_code: ref, source: "referred")
           else
-            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: ref)
+            ref = ref.split('_').map(&:capitalize).join(' ').split('?')[0]
+            param_key = postback.referral.ref.split('?')[-1].split('=')[0]
+            source = postback.referral.ref.split('?')[-1].split('=')[1]
+            if param_key == "source"
+              Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: ref, source: source)
+            else
+              Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: ref)
+            end
           end
+        else
+          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true)
         end
       else
         Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true)
