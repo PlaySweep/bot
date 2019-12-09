36348d33d61cb824f76c9a38d98a26b7b2927210
Ryan Waits <ryan.waits@gmail.com>
2019-12-09 09:06:22 -0700

    bug issue

--- bot/responses/status.rb
+++ bot/responses/status.rb
@@ -5,11 +5,12 @@ def fetch_status
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   contest_size = sweepy.latest_stats.size == 1 ? "contest" : "#{sweepy.latest_stats.size} contests"
   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
-  if sweepy.latest_stats.size >= 1
-    say "You're on a current streak of #{sweepy.stats.current_pick_streak} and have gone #{sweepy.latest_stats.map(&:wins).sum}-#{sweepy.latest_stats.map(&:losses).sum} in your last #{contest_size}.", quick_replies: quick_replies
-  else
-    say "You're just getting started, #{sweepy.first_name}! Check back once you begin collecting some stats.", quick_replies: quick_replies
-  end
+  # if sweepy.latest_stats.size >= 1
+  #   say "You're on a current streak of #{sweepy.stats.current_pick_streak} and have gone #{sweepy.latest_stats.map(&:wins).sum}-#{sweepy.latest_stats.map(&:losses).sum} in your last #{contest_size}.", quick_replies: quick_replies
+  # else
+  #   say "You're just getting started, #{sweepy.first_name}! Check back once you begin collecting some stats.", quick_replies: quick_replies
+  # end
+  say "We will be back up with your status shortly..."
   stop_thread
   
 end
