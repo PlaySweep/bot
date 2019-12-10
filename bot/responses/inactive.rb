fd1585533037e04a1babd7d6b78e7f21c1a212f8
Ryan Waits <ryan.waits@gmail.com>
2019-12-10 12:58:16 -0700

    remove button and ad link

--- bot/responses/inactive.rb
+++ bot/responses/inactive.rb
@@ -1,7 +1,7 @@
 def trigger_offseason
   sweepy = Sweep::User.find(facebook_uuid: user.id)
-  say "Thanks for playing the #{sweepy.account.app_name}, #{sweepy.first_name} - we'll be back next year!"
   url = "https://m.me/BudLightSweeps"
-  show_button("Play football", "Looking to play some football? Start playing the Bud Light Sweeps 🏈", nil, url)
+  say "Thanks for playing the #{sweepy.account.app_name}, #{sweepy.first_name} - we'll be back next year!"
+  say "Looking to play some football? Start playing the Bud Light Sweeps by following the link below 🏈\n\n#{url}"
   stop_thread
 end
