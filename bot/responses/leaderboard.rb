a43028d91b6efbc097ff2aca94469857ef2756cc
Ryan Waits <ryan.waits@gmail.com>
2020-01-23 13:26:09 -0600

    remove leaderboard

--- bot/responses/leaderboard.rb
+++ bot/responses/leaderboard.rb
@@ -5,14 +5,14 @@ def fetch_leaderboard
   # else
   #   quick_replies = [{ content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY" }, { content_type: :text, title: "Share", payload: "SHARE" }]
   # end
-  quick_replies = [{ content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY" }, { content_type: :text, title: "Share", payload: "SHARE" }]
-  if sweepy.leaderboard.account
-    if sweepy.leaderboard.account.score.to_i <= 0
-      say "You're just getting started with the #{sweepy.leaderboard.account.name}, come back once you've started earning some points!", quick_replies: quick_replies
-    else
-      say "You're currently ranked #{sweepy.leaderboard.account.rank}#{sweepy.leaderboard.account.ordinal_position} with #{sweepy.leaderboard.account.score} points in the #{sweepy.leaderboard.account.name}.", quick_replies: quick_replies
-    end
-  end
+  # quick_replies = [{ content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY" }, { content_type: :text, title: "Share", payload: "SHARE" }]
+  # if sweepy.leaderboard.account
+  #   if sweepy.leaderboard.account.score.to_i <= 0
+  #     say "You're just getting started with the #{sweepy.leaderboard.account.name}, come back once you've started earning some points!", quick_replies: quick_replies
+  #   else
+  #     say "You're currently ranked #{sweepy.leaderboard.account.rank}#{sweepy.leaderboard.account.ordinal_position} with #{sweepy.leaderboard.account.score} points in the #{sweepy.leaderboard.account.name}.", quick_replies: quick_replies
+  #   end
+  # end
   stop_thread
 end
 
