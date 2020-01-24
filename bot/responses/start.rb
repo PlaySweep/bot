5b23440663b5a242c641838c750f0df006fc485d
Ryan Waits <ryan.waits@gmail.com>
2020-01-24 14:33:03 -0600

    check postback payload for team

--- bot/responses/start.rb
+++ bot/responses/start.rb
@@ -8,7 +8,7 @@ def start
       if referral
         if referral.ad_id
           source = "ad_#{referral.ad_id}"
-          if referral.payload == "CHIEFS" || referral.payload == "49ers"
+          if postback.payload == "CHIEFS" || postback.payload == "49ers"
             team = referral.payload.capitalize
             Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team, source: source)
           else
