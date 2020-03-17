8d5c4dd8a1c3756c58ad7d32e5f44577c6f2058a
Ryan Waits <ryan.waits@gmail.com>
2020-03-17 11:04:04 -0500

    update copy

--- bot/responses/inactive.rb
+++ bot/responses/inactive.rb
@@ -2,6 +2,6 @@ def trigger_offseason
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
   date = sweepy.account.app_name == "Budweiser Sweep" ? (Date.new(2020, 3, 23) - Date.today).to_i : (Date.new(2020, 9, 10) - Date.today).to_i
-  say "The #{sweepy.account.app_name} first contests for the season will be available in #{date} days, #{sweepy.first_name}!"
+  say "The #{sweepy.account.app_name} contests will be available in #{date} days, #{sweepy.first_name}!"
   stop_thread
 end
