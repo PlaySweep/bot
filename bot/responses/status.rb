8575103d2829da3715a0d64dd25019f7cdd75973
Ryan Waits <ryan.waits@gmail.com>
2020-03-21 23:20:49 -0500

    edit status responses

--- bot/responses/status.rb
+++ bot/responses/status.rb
@@ -1,15 +1,17 @@
 def fetch_status
   sweepy = Sweep::User.find(facebook_uuid: user.id)
-  contest_size = sweepy.latest_stats.size == 1 ? "contest" : "#{sweepy.latest_stats.size} contests"
-  if sweepy.current_account_leaderboard
-    quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
-  else
-    quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
-  end
-  if sweepy.latest_stats.size >= 1
-    say "You're on a current streak of #{sweepy.stats.current_pick_streak} and have gone #{sweepy.latest_stats.map(&:wins).sum}-#{sweepy.latest_stats.map(&:losses).sum} in your last #{contest_size}.", quick_replies: quick_replies
-  else
-    say "You're just getting started, #{sweepy.first_name}! Check back once you begin collecting some stats.", quick_replies: quick_replies
-  end
+  # contest_size = sweepy.latest_stats.size == 1 ? "contest" : "#{sweepy.latest_stats.size} contests"
+  # if sweepy.current_account_leaderboard
+  #   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
+  # else
+  #   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
+  # end
+  # if sweepy.latest_stats.size >= 1
+  #   say "You're on a current streak of #{sweepy.stats.current_pick_streak} and have gone #{sweepy.latest_stats.map(&:wins).sum}-#{sweepy.latest_stats.map(&:losses).sum} in your last #{contest_size}.", quick_replies: quick_replies
+  # else
+  #   say "You're just getting started, #{sweepy.first_name}! We'll begin collecting some stats for you soon...", quick_replies: quick_replies
+  # end
+  quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Share", payload: "SHARE"}]
+  say "You're just getting started, #{sweepy.first_name}! We'll begin collecting some stats for you soon...", quick_replies: quick_replies
   stop_thread
 end
