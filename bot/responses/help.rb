adb76aa77a5220a0869426755d204158830f0b9f
Ryan Waits <ryan.waits@gmail.com>
2020-03-23 13:56:04 -0500

    change help url

--- bot/responses/help.rb
+++ bot/responses/help.rb
@@ -11,7 +11,7 @@ def help
   
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}, {content_type: :text, title: "Status", payload: "STATUS"}]
-  url= "#{ENV["WEBVIEW_URL"]}/dashboard/#{sweepy.slug}"
+  url= "#{ENV["WEBVIEW_URL"]}/customer_support/#{sweepy.slug}"
   show_button("Reach out to us!", "Let us know what's up and we'll get back to you as soon as possible.", quick_replies, url)
   stop_thread
   
