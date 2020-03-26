e5860bf9a4deea599883b09df8ed34051720abb0
Ryan Waits <ryan.waits@gmail.com>
2020-03-26 10:43:47 -0500

    update switching team copy

--- bot/responses/handle_team_selection.rb
+++ bot/responses/handle_team_selection.rb
@@ -1,10 +1,10 @@
 def switch_prompt_message
   
   sweepy = Sweep::User.find(facebook_uuid: user.id)
-  say "If you are looking to select a team or switch, keep in mind that you will be entering contests to win prizes for whichever team you select, so you may want to take into account team proximity in case tickets are the prize 👌"
+  quick_replies = [{ content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY" }, { content_type: :text, title: "Share", payload: "SHARE" }]
   text = "Tap below to select or switch teams 👇"
   url= "#{ENV["WEBVIEW_URL"]}/#{sweepy.id}/teams/initial_load"
-  show_button("Select a team", text, nil, url)
+  show_button("View teams", text, quick_replies, url)
   stop_thread
   
 end
