28b338f6f62380efcfafc86c9b70676f8456990d
Ryan Waits <ryan.waits@gmail.com>
2020-05-18 20:39:31 -0500

    change copy

--- bot/responses/rules.rb
+++ bot/responses/rules.rb
@@ -2,6 +2,6 @@ def fetch_rules
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   url = "#{ENV["WEBVIEW_URL"]}/rules"
   quick_replies = [{content_type: :text, title: "Play now", image_url: sweepy.current_team.image, payload: "PLAY"}]
-  show_button("Rules", "Tap below to view the official Budweiser Sweep rules 👇", quick_replies, url)
+  show_button("Rules", "Tap below to view the Official Rules 👇", quick_replies, url)
   stop_thread
 end
