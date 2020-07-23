9a1256778a8461136abfb4c172c225cacd5207b0
Ryan Waits <ryan.waits@gmail.com>
2020-07-23 00:16:59 -0400

    add share and switch screens

--- bot/responses/share.rb
+++ bot/responses/share.rb
@@ -2,7 +2,7 @@ def trigger_invite
   
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   quick_replies = [{content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY"}]
-  url = "#{ENV['WEBVIEW_URL']}/messenger/#{sweepy.facebook_uuid}"
+  url = "#{ENV['WEBVIEW_URL']}/status/referral"
   show_button("Refer a friend", "Share with your friends below 👇", quick_replies, url)
   stop_thread
   
