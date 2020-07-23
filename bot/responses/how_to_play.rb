ceeabe83bd8927110907687a74d61f522bcc47d1
Ryan Waits <ryan.waits@gmail.com>
2020-07-23 01:07:50 -0400

    change Budweiser Sweep to say MLB

--- bot/responses/how_to_play.rb
+++ bot/responses/how_to_play.rb
@@ -4,12 +4,20 @@ def general_how_to_play
   if sweepy.confirmed
     puts "Sweepy #{sweepy.inspect}"
     how_to_play_copy = sweepy.copies.find { |copy| copy.category == "How To Play" }
-    interpolated_how_to_play_copy = how_to_play_copy.message % { team_abbreviation: sweepy.current_team.abbreviation }
+    if sweepy.current_team_is_default
+      interpolated_how_to_play_copy = how_to_play_copy.message % { team_abbreviation: current_team.account }
+    else
+      interpolated_how_to_play_copy = how_to_play_copy.message % { team_abbreviation: sweepy.current_team.abbreviation }
+    end
     quick_replies = [{content_type: :text, title: "Play now", image_url: sweepy.current_team.image, payload: "PLAY"}]
     say interpolated_how_to_play_copy, quick_replies: quick_replies
   else
     how_to_play_copy = sweepy.copies.find { |copy| copy.category == "How To Play" }
-    interpolated_how_to_play_copy = how_to_play_copy.message % { team_abbreviation: sweepy.current_team.abbreviation }
+    if sweepy.current_team_is_default
+      interpolated_how_to_play_copy = how_to_play_copy.message % { team_abbreviation: current_team.account }
+    else
+      interpolated_how_to_play_copy = how_to_play_copy.message % { team_abbreviation: sweepy.current_team.abbreviation }
+    end
     quick_replies = [{content_type: :text, title: "Sign up now!", payload: "PLAY READY"}]
     say interpolated_how_to_play_copy, quick_replies: quick_replies
   end
