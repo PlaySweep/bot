4acc99bfe9afff25d1ecbaa7c8208a3acb8017b4
Ryan Waits <ryan.waits@gmail.com>
2020-07-27 11:30:57 -0500

    update team select url

--- bot/responses/handle_team_selection.rb
+++ bot/responses/handle_team_selection.rb
@@ -3,7 +3,7 @@ def switch_prompt_message
   sweepy = Sweep::User.find(facebook_uuid: user.id)
   quick_replies = [{ content_type: :text, title: "Play again", image_url: sweepy.current_team.image, payload: "PLAY" }, { content_type: :text, title: "Share", payload: "SHARE" }]
   text = "Tap below to select or switch teams 👇"
-  url = "#{ENV['WEBVIEW_URL']}/user/preferences"
+  url = "#{ENV['WEBVIEW_URL']}/messenger/#{sweepy.facebook_uuid}/preferences"
   show_button("View teams", text, quick_replies, url)
   stop_thread
   
