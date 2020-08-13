a2c390be20592f559bbe1a345bd05d2568dbc9ac
Ryan Waits <ryan.waits@gmail.com>
2020-08-13 09:48:37 -0500

    use #find_or_create

--- bot/responses/start.rb
+++ bot/responses/start.rb
@@ -12,44 +12,44 @@ def start
       if referral
         if referral.ad_id
           source = "ad_#{referral.ad_id}"
-          Sweep::User.create(facebook_uuid: user.id, onboard: true, source: source)
+          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: source)
         elsif referral.ref && referral.ref != ""
           if referral.ref.start_with?("referral")
             referral_code = referral.ref.split('?')[-1].split('=')[-1]
-            Sweep::User.create(facebook_uuid: user.id, onboard: true, referral_code: referral_code, source: "referred")
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, referral_code: referral_code, source: "referred")
             puts "Friend referral: #{referral_code}"
           elsif referral.ref.start_with?("fb_group")
             team = referral.ref.split('?')[0].split("_")[-1]
             source = referral.ref.split('?')[-1].split('=')[-1]
-            Sweep::User.create(facebook_uuid: user.id, onboard: true, team: team, source: source)
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: team, source: source)
             puts "Facebook Group: Team => #{team} Source => #{source}"
           elsif referral.ref.start_with?("global_fb_group")
             source = referral.ref.split('?')[-1].split('=')[-1]
-            Sweep::User.create(facebook_uuid: user.id, onboard: true, source: source)
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: source)
             puts "Global Facebook Group: Source => #{source}"
           elsif referral.ref.start_with?("global")
             source = referral.ref.split('?')[-1].split("=")[-1]
-            Sweep::User.create(facebook_uuid: user.id, onboard: true, source: source)
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: source)
           elsif referral.ref.include?("lp")
             ref = referral.ref.split('?').map(&:capitalize)[0]
             source = referral.ref.split('?')[-1].split("=")[-1]
-            Sweep::User.create(facebook_uuid: user.id, onboard: true, team: ref, source: source)
+            Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: ref, source: source)
             puts "Landing page: ref => #{ref} source => #{source}"
           else
             ref = referral.ref.split('_').map(&:capitalize)[0]
             if ref.nil?
-              Sweep::User.create(facebook_uuid: user.id, onboard: true, source: "other")
+              Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: "other")
               puts "Other: Bad Ref => #{ref}"
             else
-              Sweep::User.create(facebook_uuid: user.id, onboard: true, team: ref, source: "social_media")
+              Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, team: ref, source: "social_media")
               puts "Social Media Post: ref => #{ref}"
             end 
           end
         else
-          Sweep::User.create(facebook_uuid: user.id, onboard: true, source: "other")
+          Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: "other")
         end
       else
-        Sweep::User.create(facebook_uuid: user.id, onboard: true, source: "other")
+        Sweep::User.find_or_create(facebook_uuid: user.id, onboard: true, source: "other")
       end
     rescue NoMethodError => e
       puts "Error => #{e.inspect}\n"
